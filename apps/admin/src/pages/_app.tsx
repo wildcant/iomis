6633b5d953b495d39e5069ce21296263d807943e
Wilmer Cantillo <will.canti2697@gmail.com>
2022-10-01 17:29:02 -0500

    init iomis restaurant monorepo

---
+++ apps/admin/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import '../styles/globals.css'
+import type {AppType} from 'next/app'
+import {trpc} from '../utils/trpc'
+
+const MyApp: AppType = ({Component, pageProps}) => {
+  return <Component {...pageProps} />
+}
+
+export default trpc.withTRPC(MyApp)
---
+++ apps/admin/src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,8 @@
+import {createNextApiHandler} from '@trpc/server/adapters/next'
+import {appRouter, createContext} from '@iomis/api'
+
+// export API handler
+export default createNextApiHandler({
+  router: appRouter,
+  createContext: createContext,
+})
