c7709fd53c875cb2e37c3dd627a5414655981826
Wilmer Cantillo <will.canti2697@gmail.com>
2022-10-01 19:13:45 -0500

    update metro config to use turbo cache.

--- apps/pos/README.md
+++ apps/pos/README.md
@@ -15,7 +15,15 @@ Steps:
    This will generate the eas.json file with the configuration required to build and publish.
 2. eas build
 
-First build details at https://expo.dev/accounts/testing.wc/projects/iomis-restaurant-pos/builds/65a26cd2-20a5-4f4c-9c53-07fa2b0154b5
+First build action at https://github.com/wildcant/iomis/actions/runs/3166641411/jobs/5156489100
+
+### About Metro
+
+We leverage the Metro cache to speed up building and publishing.
+We use Turborepo to restore or invalidate this cache, working around potential
+environment variable issues. To populate this Metro cache, the apps/mobile has
+a $ pnpm build script that exports React Native bundles. The resulting Metro
+cache is then reused when publishing previews.
 
 ### Useful Links
 
--- apps/pos/metro.config.js
+++ apps/pos/metro.config.js
@@ -1,24 +1,32 @@
 // Learn more: https://docs.expo.dev/guides/monorepos/
-const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
+const { getDefaultConfig } = require('expo/metro-config')
+const { FileStore } = require('metro-cache')
+const path = require('path')
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, '../..');
+const projectRoot = __dirname
+const workspaceRoot = path.resolve(projectRoot, '../..')
 
 // Create the default Metro config
-const config = getDefaultConfig(projectRoot);
+const config = getDefaultConfig(projectRoot)
 
 // Add the additional `cjs` extension to the resolver
-config.resolver.sourceExts.push('cjs');
+config.resolver.sourceExts.push('cjs')
 
 // 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
+config.watchFolders = [workspaceRoot]
 // 2. Let Metro know where to resolve packages and in what order
 config.resolver.nodeModulesPaths = [
   path.resolve(projectRoot, 'node_modules'),
   path.resolve(workspaceRoot, 'node_modules'),
-];
+]
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+config.resolver.disableHierarchicalLookup = true
 
-module.exports = config;
+// Use turborepo to restore the cache when possible
+config.cacheStores = [
+  new FileStore({
+    root: path.join(projectRoot, 'node_modules', '.cache', 'metro'),
+  }),
+]
+
+module.exports = config
